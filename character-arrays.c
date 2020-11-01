301b06d8fa6999630170e0b977dd93f2d3053be8
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-01 10:21:37 -0600

    reverse function

--- character-arrays.c
+++ character-arrays.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
+#define MAXLINE 10
 
 int getLine(char line[], int maxline);
 void copy(char to[], char from[]);
+void reverse(char line[], int len);
 
 int getLine(char lne[], int lim){
     int c,i;
-    for(i=0; i < lim-1 && ((c = getchar()) != '.') && c!='\n'; ++i){
+    for(i=0; i < lim-1 && ((c = getchar()) != '.') && c!='\n' ; ++i){
         lne[i] =c;
         if( c ==  '\n'){
             lne[i] =c;
             ++i;
         }
     }
+    
     lne[i] = '\0';
+    reverse(lne,i);
     return i;
 }
 
 void copy(char to[], char from[]){
     int i;
     i = 0;
-    while((to[i] = from[i]) != '\0'){       //while itsn't the last character...
+    while((to[i] = from[i]) != '\0'){       //while isn't the last character...
         ++i;
     }
+}
+
+void reverse(char line[], int len){
+    printf("%d",len);
+    for(int i = len; i >= 0; i--){
+        printf("%c",line[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char const *argv[])
@@ -37,10 +48,12 @@ int main(int argc, char const *argv[])
         if(len > max){
             max = len;
             copy(longest, line);
+            
         }
+
     }
     if(max > 0){
-        printf("%s", longest);
+        printf("%s\n", longest);
     }
     return 0;
 }
